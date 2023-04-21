ed6911c9a4ee05b6f7e42cff117c2c7e6e0e872f
Vinicius Parente <vd.parente@hotmail.com>
2023-04-20 22:47:34 -0300

    input project

---
+++ Extend_Dev_LG_WebOs/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extend_Dev_LG_WebOs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string token = ConfigurationManager.AppSettings["token"];
+                if (string.IsNullOrWhiteSpace(token))
+                {
+                    Log("Token Vazio no App.config");
+                    return;
+                }
+
+                string checkURL = $"https://developer.lge.com/secure/CheckDevModeSession.dev?sessionToken={token}";
+                GetResponseSite(checkURL);
+
+                string resetDev = $"https://developer.lge.com/secure/ResetDevModeSession.dev?sessionToken={token}";
+                GetResponseSite(resetDev);
+            }
+            catch (Exception e)
+            {
+                Log(e.Message);
+                Log(e.StackTrace);
+            }
+        }
+
+        static void GetResponseSite(string url)
+        {
+
+            WebRequest wrGETURL = WebRequest.Create(url);
+            using (Stream objStream = wrGETURL.GetResponse().GetResponseStream())
+            {
+                using (StreamReader objReader = new StreamReader(objStream))
+                {
+                    string sLine = objReader.ReadLine();
+                    if (sLine != null)
+                        Log($"{DateTime.Now} -  {sLine}");
+                }
+            }
+        }
+
+        static void Log(string str)
+        {
+            Console.WriteLine(str);
+            File.WriteAllText("log.txt", str);
+        }
+    }
+}
---
+++ Extend_Dev_LG_WebOs/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Extend_Dev_LG_WebOs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Extend_Dev_LG_WebOs")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("d617b3dc-9873-4046-bae9-c0622f63c16c")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
